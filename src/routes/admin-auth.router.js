98eab3da64854c90c87ef4c3a51bd20c52719e8b
Udhaykrishn <krishnauday320@gmail.com>
2025-03-24 09:45:11 +0530

    feat: remove comments

--- src/routes/admin-auth.router.js
+++ src/routes/admin-auth.router.js
@@ -7,28 +7,24 @@ import {
     createMatch,
     updateMatch,
     getMatches,
-    generateFixturesForTournament // Include this since it’s defined in the controller
+    generateFixturesForTournament
 } from "../controllers/tournament-match.controller.js";
 import { adminMiddleware } from "../middleware/admin.middleware.js";
 
 const router = Router();
 
-// Auth routes
 router.post("/login", loginAdmin);
 router.post("/logout", logoutAdmin);
 router.get("/profile", adminMiddleware, (req, res) => res.status(200).json({ message: "this is admin protected route" }));
 
-// Tournament routes
 router.post("/tournaments", adminMiddleware, createTournament);
 router.put("/tournaments/:id", adminMiddleware, updateTournament);
 router.get("/tournaments", adminMiddleware, getTournaments);
 
-// Match routes
 router.post("/matches", adminMiddleware, createMatch);
-router.put("/matches/:matchId", adminMiddleware, updateMatch); // Fixed to include both tournamentId and matchId
+router.put("/matches/:matchId", adminMiddleware, updateMatch); 
 router.get("/matches", adminMiddleware, getMatches);
 
-// Fixture generation route
 router.post("/tournaments/:id/generate-fixtures", adminMiddleware, generateFixturesForTournament);
 
 export default router;
